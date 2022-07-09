e2a0a14ab159ab571cd993caaed5ace5822ff13e
RuzaKansakar <ruzakansakar@gmail.com>
2022-07-09 11:25:38 +0545

    Added html and css.

---
+++ introtojs.js
@@ -0,0 +1,55 @@
+// let name="CSIT"
+// let number=1;
+// let isNice=true;
+// console.log(name);
+
+// function power(28)
+// {
+//     return 2*28
+// }
+// power(2)
+// console.log(power(2));
+// let result=power();
+
+// const listOfPeople=["name",1,]
+
+//  const students={
+//      name:"Ru",
+//      age:20,
+//      address:"sw"
+//  }
+
+//  //object.assign()
+//  console.log(students.age);
+
+//  //!Jason file
+//  const peoples=
+//  [
+//      {
+//          name:"Ru",
+//         age:20,
+//     },
+// {
+//     ...
+// }
+//  ]
+
+// document.rootElement
+
+// const body=document.body
+// const headNode=document.createElement("h1");
+// const textNode=document.createTextNode("title");
+// document.insertBefore()
+
+// document.getElementsById
+// document.addEventListener("click",function(event))
+// {
+//     if(event.keycode="S")
+//     {
+//         player.move(left)
+//     }
+// }
+
+// import index from './Index.html';
+// import React from ''
+
